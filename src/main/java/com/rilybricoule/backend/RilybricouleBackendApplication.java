983dfd3f841933e7abee13d07eeaf789d19ca527
Hatim Harchane <hharchane1@gmail.com>
2026-01-28 15:47:54 +0000

    Initialisation du projet springboot

---
+++ src/main/java/com/rilybricoule/backend/RilybricouleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rilybricoule.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RilybricouleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RilybricouleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rilybricoule/backend/RilybricouleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rilybricoule.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RilybricouleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
