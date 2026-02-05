451f37aba3d2669bb58364d1310c6badde09e500
soufyaneakouri <soufyane.akouri@e-polytechnique.ma>
2026-02-05 21:40:33 +0100

    Save current changes before switching branch

---
+++ IdeaProjects/RilyBricoule/src/main/java/com/example/rilybricoule/RilyBricouleApplication.java
@@ -0,0 +1,13 @@
+package com.example.rilybricoule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RilyBricouleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RilyBricouleApplication.class, args);
+    }
+
+}
